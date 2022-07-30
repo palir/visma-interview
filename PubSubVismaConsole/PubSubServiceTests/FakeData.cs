a07b62d6259b904e92f56a22cf5658e285d4994e
pali <pavol.raska@gmail.com>
2022-07-30 18:53:42 +0200

    fixes

---
+++ PubSubVismaConsole/PubSubServiceTests/FakeData.cs
@@ -0,0 +1,27 @@
+﻿using PubSubService.DataClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PubSubServiceTests
+{
+    public class FakeData
+    {
+        public static DateTime StartDate => new DateTime(2022, 9, 10, 17, 0, 0);
+
+        public static Weather Weather => new Weather()
+        {
+            AirTemperature = 20,
+            Description = "Cloudy"
+        };
+
+        public static CultureEvent CultureEvent => new CultureEvent()
+        {
+            Name = "Conncert",
+            Place = "Arena",
+            BeginsAt = StartDate
+        };
+    }
+}
--- PubSubVismaConsole/PubSubServiceTests/PublisherTests.cs
+++ PubSubVismaConsole/PubSubServiceTests/PublisherTests.cs
@@ -1,4 +1,5 @@
 using NSubstitute;
+using PubSubService.DataClasses;
 using PubSubService.Interfaces;
 using PubSubService.Services;
 
@@ -17,9 +18,29 @@ namespace PubSubServiceTests
         }
 
         [Test]
-        public void Test1()
+        public void Publish_NullMessage_ThrowsException()
         {
-            Assert.Pass();
+            // Arrange
+
+            // Act
+            var ex = Assert.Throws<ArgumentNullException>(delegate { this.target.Publish<Weather>((Message<Weather>)null); });
+
+            // Assert
+            Assert.That(ex, Is.Not.Null, "Exception was null");
+            Assert.That(ex.Message, Does.Contain("message"), "Expected parameter name not returned in error");
+        }
+
+        [Test]
+        public void Publish_ValidMessage_CallsSendOnBroker()
+        {
+            // Arrange
+            Message<Weather> message = new Message<Weather>(FakeData.Weather);
+
+            // Act
+            this.target.Publish<Weather>(message);
+
+            // Assert
+            this.messagebroker.Received(1).SendMessage<Weather>(message);
         }
     }
 }
